6872848c5cb1c975be29a0dab4976e78070cd09e
Aashi-Jha <82762978+Aashi-Jha@users.noreply.github.com>
2021-08-10 18:10:17 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function login(){
+    player1_name=document.getElementById("player_1").value;
+    player2_name=document.getElementById("player_2").value;
+
+    localStorage.setItem("player_1" , player1_name);
+    localStorage.setItem("player_2" , player2_name);
+
+    window.location="game_screen.html";
+}
